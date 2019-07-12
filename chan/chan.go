ef0152364486bd4291d30cfae83ebad7354cd329
Tenlu <tenlu@itech8.com>
2019-07-12 18:15:08 +0800

    fix

--- chan/chan.go
+++ chan/chan.go
@@ -9,23 +9,55 @@ import "fmt"
 4.nil、buffered、closed
 5.
 */
+
 func main() {
+	defer func() {
+		//if err:=recover();err!=nil{
+		//	fmt.Println(err)
+		//}
+		errMsg:=recover()
+		fmt.Println(errMsg)
+	}()
 	var status chan int
 	if status == nil {
 		fmt.Println("未初始化的chan 值是nil")
 	}
-	status = make(chan int, 10)
+	status = make(chan int,3)
+
 	fmt.Println("init value:", status)
 	status <- 1
-	fmt.Println("status value:", <-status)
+	status <- 2
+	status <- 3
+	status <- 4
+	// 当超过容量值就回抛出错误
+	close(status)
+
+	for {
+		value, ok := <-status
+		if !ok {
+			fmt.Println("status chan closed!")
+			break
+		}
+		fmt.Println("status value:",value)
+	}
 
 	pingc := make(chan string, 1)
 	pongc := make(chan string, 1)
 
-
-	ping(pingc, "i'm comming")
-	pong(pingc, pongc)
+	go ping(pingc, "i'm comming")
+	go pong(pingc, pongc)
 	fmt.Println(<-pongc)
+
+	ch1:=make(chan int,10)
+	ch2:=make(chan int,10)
+
+	done:=make(chan bool)
+
+	go sendChan(ch1,done)
+	go reveChan(ch1,ch2,done)
+
+	<-done
+	<-done
 }
 
 func ping(pingc chan<- string, msg string) {
@@ -35,3 +67,26 @@ func pong(pings <-chan string, pongs chan<- string) {
 	msg := <-pings
 	pongs <- msg
 }
+
+func sendChan(ch1 chan int,done chan bool)  {
+	fmt.Println("------sendChan--------")
+
+	for i:=1;i<=10;i++{
+		ch1<-i
+	}
+	close(ch1)
+	done<-true
+}
+
+func reveChan(ch1 chan int,ch2 chan int,done chan bool)  {
+	fmt.Println("------receChan--------")
+	for c:=range ch1 {
+		ch2<-c
+		fmt.Println("ch1:",c)
+	}
+	if _,ok:=<-ch1;!ok{
+		fmt.Println("ch1 is closed")
+	}
+	done<-true
+}
+
