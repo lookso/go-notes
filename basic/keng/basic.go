d8b4bcb2b93e145d30f5d5eff6b12e2dbbcca9d9
lk <505719081@qq.com>
2019-09-22 23:20:30 +0800

    add basic

---
+++ basic/keng/basic.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := []int{1, 2, 3, 4}
+	b := a[:]
+	a = append(a, 5)
+	b[0] = 0
+	fmt.Println(cap(a), cap(b), a, b)
+	// 8 4 [1 2 3 4 5] [0 2 3 4]
+
+	fmt.Println(split(5))
+	// map
+	strMap := map[int]string{1: "a", 2: "b", 3:
+	"c"}
+	strMap[4] = "d"
+	delete(strMap, 0) // map 不能指定索引删除,需要指定key 来删除
+	// delete(strMap,1)
+	fmt.Printf("%v\n", strMap)
+
+	aa := "012"
+	pa := &aa // 0xc000092030
+	bb := []byte(*pa) // []byte("012")
+	pb := &bb
+
+	aa += "3"
+	*pa += "4"
+	bb[1] = '5'
+
+	println(*pa) // 01234
+	println(string(*pb)) // 052
+}
+
+func split(sum int) (x, y, z int) {
+	x = sum * 4 / 9 // 因为x 是整形 int 结果是2
+	y = sum - x     // 3
+	z = x + y       // 5
+	return
+}
