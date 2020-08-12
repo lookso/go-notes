fcfff61a910d4b60aa2343290aa3a6ff8a69099d
lukun <lukun@cmcm.com>
2020-08-12 19:51:35 +0800

    fix context

--- pkg/context.go
+++ pkg/context.go
@@ -2,22 +2,44 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"time"
+	"net/http"
 )
 
-func main() {
-	// 根节点
-	rootCtx := context.Background()
-	// 子节点
-	ctx1 := context.WithValue(rootCtx, "name", "jack")
-	// 子子节点,一层层向下传递
-	ctx2, cancel := context.WithCancel(ctx1)
-	defer cancel()
-	name, ok := ctx2.Value("name").(string)
-	if ok {
-		fmt.Println(name)
+type FooKey string
+
+var UserName = FooKey("user-name")
+var UserId = FooKey("user-id")
+
+func nextHandle(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := context.WithValue(r.Context(), UserId, "100861")
+		ctx2 := context.WithValue(ctx, UserName, "peanut.com")
+		next(w, r.WithContext(ctx2))
 	}
+}
 
+func GetUserName(context context.Context) string {
+	if ret, ok := context.Value(UserName).(string); ok {
+		return ret
+	}
+	return ""
+}
 
+func GetUserId(context context.Context) string {
+	if ret, ok := context.Value(UserId).(string); ok {
+		return ret
+	}
+	return ""
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("welcome: "))
+	w.Write([]byte(GetUserId(r.Context())))
+	w.Write([]byte(" "))
+	w.Write([]byte(GetUserName(r.Context())))
+}
+
+func main() {
+	http.Handle("/", nextHandle(index))
+	http.ListenAndServe(":8080", nil)
 }
