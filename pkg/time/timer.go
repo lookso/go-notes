fae22ea4f919a39fd0fc9ad8d13dd4812ee759eb
lukun <lukun@cmcm.com>
2020-08-21 11:26:49 +0800

    fix 定时器

---
+++ pkg/time/timer.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// 定时器
+func newTimer() {
+	t := time.NewTimer(2 * time.Second)
+	done := make(chan bool)
+	go func(tm *time.Timer) {
+		defer tm.Stop()
+		for {
+			select {
+			case <-t.C:
+				fmt.Println("timer running....")
+				t.Reset(2 * time.Second)
+			case _, ok := <-done:
+				if ok {
+					fmt.Println("game again comming....")
+					break
+				}
+			}
+		}
+	}(t)
+	done <- true
+	close(done)
+	select {}
+}
+func newTicker() {
+	t := time.NewTicker(2 * time.Second)
+	fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
+	defer t.Stop()
+	aft := <-t.C
+	fmt.Println(aft.Format("2006-01-02 15:04:05"))
+}
+func newAfter() {
+	t := time.After(time.Second * 3)
+	fmt.Printf("t type=%T\n", t)
+	//阻塞3秒
+	af := <-t
+	fmt.Println("t=", af.Format("2006-01-02 15:04:05"))
+}
+
+func main() {
+	newAfter()
+	newTicker()
+	newTimer()
+}
