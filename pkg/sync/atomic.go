e92d7ee8245039c653d607971a586c571c8be3fc
lukun <lukun@cmcm.com>
2020-09-04 10:06:32 +0800

    fix pkg

--- pkg/sync/atomic.go
+++ pkg/sync/atomic.go
@@ -7,6 +7,19 @@ import (
 
 func main() {
 	var num int32
-	nv:=atomic.AddInt32(&num, 1)
+	nv := atomic.AddInt32(&num, -1)
 	fmt.Println(nv)
+	var old int32
+	var new int32
+	var addr int32
+	addr = 1
+	old = 1
+	new = 2
+	// CAS 比较交换,乐观锁版本号的概念
+	// 如果addr和old相同,就用new代替addr
+	if atomic.CompareAndSwapInt32(&addr, old, new) {
+		fmt.Println("old", old)
+		fmt.Println("new", new)
+		fmt.Println("addr", addr)
+	}
 }
