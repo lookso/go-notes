313a217462fc773b522235a56a3992e9ddd805fd
lukun <lukun@cmcm.com>
2020-09-24 16:45:22 +0800

    add http

--- pkg/net/http.go
+++ pkg/net/http.go
@@ -1,1 +1,18 @@
-package net
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	log.Println("请求来了", r.URL)
+	fmt.Println(w.Write([]byte("hello cmcm")))
+}
+func main() {
+	http.HandleFunc("/index", index)
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		fmt.Println(err)
+	}
+}
