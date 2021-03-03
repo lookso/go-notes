e2e709c279c94e78d42b4588ada45909512e78cc
lukun <lukun@cmcm.com>
2021-03-03 10:41:50 +0800

    fix lua

--- assembly/redis/lua.go
+++ assembly/redis/lua.go
@@ -9,6 +9,6 @@ func main() {
 	client, _ := common.RedisClient()
 	// set key1 first
 	// set key2 second
-	res, err := client.Eval("return redis.call('MSET', KEYS[1],ARGV[1],KEYS[2],ARGV[2])", []string{"key1", "key2"}, "first", "second").Result()
+	res, err := client.Eval("redis.call('MSET', KEYS[1],ARGV[1],KEYS[2],ARGV[2]) return redis.call('MGET', KEYS[1],KEYS[2])", []string{"key1", "key2"}, "first", "second").Result()
 	fmt.Println(res, err)
 }
