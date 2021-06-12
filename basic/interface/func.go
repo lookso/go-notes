f4ce8ba1a2939043cbcf928180e8885866bed436
peanut <peanut@itech8.com>
2021-06-12 19:48:13 +0800

    fix func

--- basic/interface/func.go
+++ basic/interface/func.go
@@ -3,27 +3,27 @@ package main
 import "fmt"
 
 type Action interface {
-	Run() string
+	UserInfo() string
 	GetDay() int
 	SetDays(day int) func() int // 生活了多少天
 }
 
-type MyDo struct {
+type MySelf struct {
 	Height int `json:"height"`
 	Days   int `json:"days"`
 }
 
-func (*MyDo) Run() string {
+func (m *MySelf) UserInfo() string {
 	panic("implement me")
 }
 
-func (m *MyDo) GetDay() int {
+func (m *MySelf) GetDay() int {
 	return m.Days
 }
 
-func (m *MyDo) SetDays(day int) func() int {
+func (m *MySelf) SetDays(day int) func() int {
 	f := func() int {
-		m.Days = day * m.Height
+		m.Days = m.Days+day * m.Height
 		return m.Days
 	}
 	f()
@@ -31,7 +31,7 @@ func (m *MyDo) SetDays(day int) func() int {
 }
 
 func NewMyDo() Action {
-	return &MyDo{Height: 110}
+	return &MySelf{Height: 110,Days:10}
 }
 
 func main() {
