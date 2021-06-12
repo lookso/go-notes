f1182bf4131f711e9fcdae8538dad0ff681f2f1d
peanut <peanut@itech8.com>
2021-06-12 20:44:48 +0800

    fix func

--- basic/interface/func.go
+++ basic/interface/func.go
@@ -1,42 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-type Action interface {
-	UserInfo() string
+type IOurs interface {
+	She() string
+}
+
+type Our struct {
+	Class string `json:"class"`
+}
+
+func (o *Our) She() string {
+	return o.Class
+}
+
+func NewOurs(class string) IOurs {
+	return &Our{
+		Class: class,
+	}
+}
+
+type IMy interface {
+	Human(o IOurs) string
 	GetDay() int
 	SetDays(day int) func() int // 生活了多少天
 }
-
-type MySelf struct {
+type My struct {
 	Height int `json:"height"`
 	Days   int `json:"days"`
 }
 
-func (m *MySelf) UserInfo() string {
-	panic("implement me")
+func (m *My) Human(ours IOurs) string {
+	return ours.She() +":"+ strconv.Itoa(m.Days)
 }
 
-func (m *MySelf) GetDay() int {
+func (m *My) GetDay() int {
 	return m.Days
 }
 
-func (m *MySelf) SetDays(day int) func() int {
+func (m *My) SetDays(day int) func() int {
 	f := func() int {
-		m.Days = m.Days+day * m.Height
+		m.Days = m.Days + day*m.Height
 		return m.Days
 	}
 	f()
 	return f
 }
 
-func NewMyDo() Action {
-	return &MySelf{Height: 110,Days:10}
+func NewMy() IMy {
+	return &My{Height: 110, Days: 10}
+}
+
+func Your(my IMy) {
+	fmt.Println(my.GetDay())
 }
 
 func main() {
-	md := NewMyDo()
+	md := NewMy()
 	md.SetDays(22)
 	fmt.Println(md.GetDay())
+
+	Your(md)
+	fmt.Println(md.Human(NewOurs("she")))
 
 }
