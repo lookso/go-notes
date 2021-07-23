7c7a573ac20eb798005514223227eee48a4d2ca0
lukun1 <lukun1@tal.com>
2021-07-23 17:37:13 +0800

    add makefile

---
+++ Makefile/cmd/main.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+func main() {
+	t := time.NewTimer(2 * time.Second)
+	count := 0
+	done := make(chan bool)
+	go func(tm *time.Timer, count int) {
+		defer tm.Stop()
+		for {
+			select {
+			case <-t.C:
+				count++
+				fmt.Println("timer running...." + strconv.Itoa(count))
+				t.Reset(2 * time.Second)
+			case _, ok := <-done:
+				if ok {
+					fmt.Println("game again ComMing....")
+					break
+				}
+			}
+		}
+	}(t, count)
+	done <- true
+	close(done)
+	select {}
+}
--- package/rand/rand.go
+++ package/rand/rand.go
@@ -9,12 +9,13 @@ import (
 func randNew() {
 	rand.New(rand.NewSource(time.Now().UnixNano()))
 	for i := 1; i <= 20; i++ {
-		fmt.Println(rand.Intn(i))  // 0到i,i不能为0
+		fmt.Println(rand.Intn(i)) // 0到i,i不能为0
 	}
 }
+
 func randSeed() {
 	rand.Seed(time.Now().UnixNano())
 	for i := 1; i <= 20; i++ {
-		fmt.Println(rand.Intn(i))  // 0到i,i不能为0
+		fmt.Println(rand.Intn(i)) // 0到i,i不能为0
 	}
 }
