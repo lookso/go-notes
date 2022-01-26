73100ea9e78b2684391711261a041f65f6e5d272
peanut <lukun1@tal.com>
2022-01-26 18:34:55 +0800

    spew

--- debug/spew.go
+++ debug/spew.go
@@ -12,6 +12,6 @@ type Demo struct {
 
 func main() {
 	arr := [...]*Demo{{100, "Python"}, {200, "Golang"}}
-	fmt.Printf("%v\n-----------------分割线-----------\n", arr)
+	fmt.Printf("%v\n-------------------分割线-----------\n", arr)
 	spew.Dump(arr)
 }
