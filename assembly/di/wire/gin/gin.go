6dfed76510331945b6dd38c11ce8e48a23d675b9
peanut <lukun1@tal.com>
2022-07-09 15:50:28 +0800

    add blot

---
+++ assembly/di/wire/gin/gin.go
@@ -0,0 +1,12 @@
+package gin
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/ginS"
+)
+
+func main()  {
+	r:=gin.Default()
+
+
+}
--- temp/temp.go
+++ temp/temp.go
@@ -218,6 +218,15 @@ func callfunNum() {
 }
 
 func main() {
+
+	contentTextTypeCount:=0
+	for i:=0;i<10;i++{
+		if i%2==0 {
+			contentTextTypeCount++
+		}
+	}
+	fmt.Println("contentTextTypeCount",contentTextTypeCount)
+
 	callfunNum()
 
 	//var wx = WxInfo{
